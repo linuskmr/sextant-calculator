4f2882c38f1f520c4a1f4533156c237964918ba0
linuskmr <56581587+linuskmr@users.noreply.github.com>
2021-05-27 17:40:35 +0200

    Split code into its own files

---
+++ Hemisphere.ts
@@ -0,0 +1,9 @@
+/**
+ * A hemisphere / cardinal direction.
+ */
+export enum Hemisphere {
+    North = 'N',
+    East = 'E',
+    South = 'S',
+    West = 'W',
+}
---
+++ Position.ts
@@ -0,0 +1,30 @@
+import { Coordinate } from "./Coordinate"
+import { Hemisphere } from "./Hemisphere"
+
+/**
+ * A position on the globe determined by the latitude and longitude.
+ */
+export class Position {
+    readonly latitude: Coordinate
+    readonly longitude: Coordinate
+
+    /**
+     * Creates a new Position.
+     * @param latitude The latitude.
+     * @param longitude The longitude.
+     */
+    constructor(latitude: Coordinate, longitude: Coordinate) {
+        console.assert(latitude.hemisphere == Hemisphere.North || latitude.hemisphere == Hemisphere.South)
+        console.assert(longitude.hemisphere == Hemisphere.West || longitude.hemisphere == Hemisphere.East)
+        this.latitude = latitude
+        this.longitude = longitude
+    }
+
+    /**
+     * Converts this position into a string of its coordinates.
+     * @return A string in the form "latitude\nlongitude" in their representation.
+     */
+    toString(): string {
+        return `${this.latitude.toString()}\n${this.longitude.toString()}`
+    }
+}
