14df366aba99dcc0e3ab46e00eefa4ba3f6ab09e
linuskmr <56581587+linuskmr@users.noreply.github.com>
2021-09-20 17:04:58 +0200

    Add Hemisphere.fromString()

--- Hemisphere.ts
+++ Hemisphere.ts
@@ -7,3 +7,22 @@ export enum Hemisphere {
     South = 'S',
     West = 'W',
 }
+
+export function fromString(hemisphere: String): Hemisphere {
+    switch (hemisphere) {
+        case "North":
+        case "N":
+            return Hemisphere.North
+        case "East":
+        case "E":
+            return Hemisphere.East
+        case "South":
+        case "S":
+            return Hemisphere.South
+        case "West":
+        case "W":
+            return Hemisphere.East
+        default:
+            throw TypeError("Invalid hemisphere")
+    }
+}
