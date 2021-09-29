60bbfcee2846deb793edc3303c5fca4ffadbbf93
linuskmr <56581587+linuskmr@users.noreply.github.com>
2021-09-29 17:53:16 +0200

    Add a footer

---
+++ src/Footer.svelte
@@ -0,0 +1,34 @@
+<footer>
+    <p>
+        <strong>Sextant Calculator — v0.0.1</strong><br>
+        <small>by <a href="https://github.com/linuskmr">linuskmr</a></small>
+    </p>
+    <ul>
+        <li>👷 Build with <a href="https://svelte.dev">Svelte</a></li>
+        <li>🚀 Deployed with <a href="https://vercel.com/">Vercel</a></li>
+        <li>📝 Sourcecode on <a href="https://github.com/linuskmr/sextant-calculator">GitHub</a></li>
+    </ul>
+</footer>
+
+<style>
+    footer {
+        margin: 0;
+        background-color: #30363c;
+        color: white;
+        padding: 20px;
+    }
+
+    footer a {
+        color: white;
+        text-decoration: underline;
+    }
+
+    footer ul {
+        list-style: none;
+        padding: 0;
+    }
+
+    footer li {
+        padding: 10px 0 10px 0;
+    }
+</style>
