aec8848c4be7ab78f90cc9c8271b7898ced9885d
linuskmr <56581587+linuskmr@users.noreply.github.com>
2021-09-29 17:53:10 +0200

    Add sections

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
     import {Sextant} from "./lib/sextant";
     import {Angle} from "./lib/Angle";
+    import Footer from "./Footer.svelte";
 
-    let name = "Linus"
     let measured_angle = 10
-
     let date = new Date().toISOString().substring(0, 10) // https://stackoverflow.com/a/14246394
-
     $: sextant = new Sextant(new Date(), new Angle(measured_angle), Angle.zero, Angle.zero)
 
 </script>
@@ -14,26 +12,47 @@
 <main>
     <h1>Sextant Calculator</h1>
 
-    <label>
-        Measuring Date
-        <br>
-        <input type="date" bind:value={date}>
-    </label>
+    <section>
+        <label>
+            Measuring Date
+            <br>
+            <input type="date" bind:value={date}>
+        </label>
+    </section>
 
-    <label>
-        Measured Angle
-        <br>
-        <input type="number" bind:value={measured_angle}>
-    </label>
+    <section>
+        <label>
+            Measured Angle
+            <br>
+            <input type="number" bind:value={measured_angle}>
+        </label>
+    </section>
 
-    <span>Position</span>
-    <pre style="width: max-content">{sextant.position}</pre>
+    <section>
+        <span>Position</span>
+        <pre style="width: max-content">{sextant.position}</pre>
 
-    <a href="https://www.google.de/maps/place/{sextant.position.latitude},{sextant.position.longitude}">
-        Open on Google Maps
-    </a>
+        <a href="https://www.google.de/maps/place/{sextant.position.latitude},{sextant.position.longitude}">
+            Open position on Google Maps
+        </a>
+    </section>
 </main>
 
-<footer>
-    Abc
-</footer>
+<Footer/>
+
+<style>
+    pre {
+        background-color: #30363c;
+        padding: 10px;
+        border-radius: 5px;
+    }
+
+    main {
+        /* Causes the footer to appear only when scrolling down, since main always takes up at least one full page */
+        min-height: 100vh;
+    }
+
+    section {
+        padding-bottom: 15px;
+    }
+</style>
